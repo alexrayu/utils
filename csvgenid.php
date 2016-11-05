2df0f1ab534a859dd3855df65d501d0888375137
alexrayu <alex.rayu@gmail.com>
2016-11-05 14:53:22 +0200

    Add chosen. Add image select. Update libs. Add drupal console.

--- csvgenid.php
+++ csvgenid.php
@@ -5,13 +5,13 @@
  *  CSV Generate ID is a tool to generate unique ids per row.
  */
 
-define('KEY_ID', 'lid');
+define('KEY_ID', 'mid');
 define('FOLDER', '');
 
 include 'common.php';
 
 // CSV Files. First will be used as base.
-$source = FOLDER . 'input.csv';
+$source = FOLDER . 'message.csv';
 $output_file_name = FOLDER . 'output.csv';
 $header = [];
 $data = [];
